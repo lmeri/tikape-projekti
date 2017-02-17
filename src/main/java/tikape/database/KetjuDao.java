1a65d9fc448e2490855a140b7c889cf625ba490e
heli <heli.poutiainen@helsinki.fi>
2017-02-17 14:48:58 +0200

    KetjuDao:n muokkaus

--- src/main/java/tikape/database/KetjuDao.java
+++ src/main/java/tikape/database/KetjuDao.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import tikape.domain.Ketju;
+import tikape.domain.Viesti;
 
 public class KetjuDao implements Dao<Ketju, Integer> {
 
@@ -62,6 +64,35 @@ public class KetjuDao implements Dao<Ketju, Integer> {
         connection.close();
 
         return ketjut;
+    }
+
+    public List<Ketju> getLastTen() throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Ketju WHERE alue = ? ORDER BY id_tunnus DESC LIMIT 10");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Ketju> ketjut = new ArrayList<>();
+        while (rs.next()) {
+            Integer id_tunnus = rs.getInt("id_tunnus");
+            String nimi = rs.getString("nimi");
+            Integer alue = rs.getInt("alue");
+
+            ketjut.add(new Ketju(id_tunnus, nimi, alue));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return ketjut;
+    }
+
+    public void insertKetju(String nimi, Integer alue) throws SQLException {
+        Connection connection = database.getConnection();
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("INSERT INTO Ketju (nimi, alue) VALUES ('" + nimi + "', " + alue + ")");
+        stmt.close();
+        connection.close();
     }
 
     @Override
